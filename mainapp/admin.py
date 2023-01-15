101fe1da53e9bc534e1badac96be8f596eac72da
danilchik22 <85504849+danilchik22@users.noreply.github.com>
2023-01-15 20:57:09 +0700

    Lesson 6 (#4)
    * lesson_6 Link to admin
    
    * lesson_6 Customizing admin panel
    
    * lesson_6 New user register form
    
    * lesson_6 User edit form
    
    * lesson_6 News CRUD and permissions
    
    * lesson_6 Feedbacks with AJAX

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,1 +1,28 @@
-# Register your models here.
+from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+
+from mainapp import models as mainapp_models
+
+
+@admin.register(mainapp_models.News)
+class NewsAdmin(admin.ModelAdmin):
+    search_fields = ["title", "preambule", "body"]
+
+
+@admin.register(mainapp_models.Lesson)
+class Lesson(admin.ModelAdmin):
+    list_display = ["course", "title", "description", "deleted"]
+    ordering = ["-course"]
+    list_filter = ["course", "title"]
+    list_per_page = 5
+    actions = ["mark_deleted", "mark_not_deleted"]
+
+    def mark_deleted(self, request, queryset):
+        queryset.update(deleted=True)
+
+    mark_deleted.short_description = _("Mark deleted")
+
+    def mark_not_deleted(self, request, queryset):
+        queryset.update(deleted=False)
+
+    mark_not_deleted.short_description = _("Mark not deleted")
